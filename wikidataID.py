3edc914bf2ae3159e1c6d768ca939382ccdc5d56
Bill Griffin <151380488+BillGriffin98@users.noreply.github.com>
2024-03-04 12:05:47 +0000

    Add comments to wikidataID.py

--- wikidataID.py
+++ wikidataID.py
@@ -1,30 +1,38 @@
-import requests
+import requests  # Importing requests for making HTTP requests
 
 def get_wikidata_id_from_wikipedia(wikipedia_title):
+    """
+    Retrieves the Wikidata ID associated with a Wikipedia article.
+
+    Args:
+        wikipedia_title (str): The title of the Wikipedia article.
+
+    Returns:
+        str or None: The Wikidata ID if found, else None.
+    """
     # API endpoint for MediaWiki
     mediawiki_api_url = 'https://en.wikipedia.org/w/api.php'
     
     # Parameters for the API request
     params = {
-        'action': 'query',
-        'titles': wikipedia_title,
-        'prop': 'pageprops',
-        'format': 'json',
-        'redirects': 'true'
+        'action': 'query',        # Action to perform (query)
+        'titles': wikipedia_title,  # Title of the Wikipedia article
+        'prop': 'pageprops',      # Property to fetch (pageprops)
+        'format': 'json',         # Response format (JSON)
+        'redirects': 'true'       # Follow redirects
     }
     
     # Make the API request
-    response = requests.get(mediawiki_api_url, params=params)
-    data = response.json()
+    response = requests.get(mediawiki_api_url, params=params)  # Sending a GET request to the MediaWiki API
+    data = response.json()  # Converting the response to JSON format
     
     # Extract Wikidata ID from the response
     if 'query' in data and 'pages' in data['query']:
         for page_id, page_info in data['query']['pages'].items():
             if 'pageprops' in page_info and 'wikibase_item' in page_info['pageprops']:
-                wikidata_id = page_info['pageprops']['wikibase_item']
-                return wikidata_id
+                # If 'wikibase_item' is found in 'pageprops', it contains the Wikidata ID
+                wikidata_id = page_info['pageprops']['wikibase_item']  # Extracting the Wikidata ID
+                return wikidata_id  # Returning the Wikidata ID
     
     # Return None if Wikidata ID is not found
     return None
-
-
